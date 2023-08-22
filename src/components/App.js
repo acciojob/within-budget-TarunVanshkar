908f66fdaa7f68acc3d581eaa8182f838f41aa77
TarunVanshkar <tarunben33@gmail.com>
2023-08-22 21:09:45 +0530

    submitting code-Tue Aug 22 2023 21:09:36 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,52 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const itemList = [
+  { item: 'Apple', price: 20 },
+  { item: 'Banana', price: 10 },
+  { item: 'Neutella', price: 30 },
+  { item: 'Razor', price: 5 },
+  { item: 'CornFlakes', price: 15 },
+  { item: 'Sound Bar', price: 50 },
+  { item: 'Iphone', price: 80 },
+]
 const App = () => {
+  const [value, setValue] = useState(0);
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Enter your budget to check available items:</h1>
+      <input type="number" value={value} onChange={(e) => setValue(e.target.value)} />
+      <table>
+        <thead>Items you can buy are in Green color</thead>
+        <tbody>
+          {
+            itemList.map((product) => {
+              return (
+                <tr>
+                  <td>{product.item}</td>
+                  <td style={{ color: product.price > value ? 'red' : 'green' }}>
+                    {product.price}
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
 
 export default App
+
+// {
+//   itemList.map((element, index) => {
+//     if(element.price <= value){
+//       return <li key={index}>{element.item}</li>
+//     }
+//     else{
+//       return <li key={index}>{element.item}</li>
+//     }
+//   })
+// }
